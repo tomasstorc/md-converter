1d7a368c8218379595b6828b324b20559ed90902
Tomas Storc <tomas.storc@gmail.com>
2023-08-02 14:38:06 +0200

    check for headers

--- ts-mdconverter-func/__init__.py
+++ ts-mdconverter-func/__init__.py
@@ -17,19 +17,22 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         for file in req.files.values():
             dToOut = convert2html(file.stream.read())
             return func.HttpResponse(json.dumps(dToOut) , mimetype="application/json", status_code=200)
-    # req_body = req.get_json()
-    # if req_body:
-    #     data = req_body.get('data')
-    #     if data:
-    #         converted = convert2html(data)
-    #         dToOut = {
-    #             "data": converted,
-    #             "status": "success"
-    #         }
-    #         return func.HttpResponse(json.dumps(dToOut) , mimetype="application/json", status_code=200)
-    # else:
-    #     return func.HttpResponse('no daa found')
+    if req.headers.get('Content-Type') == "application/json":
+        req_body = req.get_json()
+        if req_body:
+            data = req_body.get('data')
+            if data:
+                converted = convert2html(data)
+                dToOut = {
+                    "data": converted,
+                    "status": "success"
+                }
+                return func.HttpResponse(json.dumps(dToOut) , mimetype="application/json", status_code=200)
+        else:
+            return func.HttpResponse('no daa found')
     if len(req.files.values()) == 0:
         return func.HttpResponse("No content", status_code=400)
+    else:
+        return func.HttpResponse("here")
     
     
